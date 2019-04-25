e67642e21801119df51d0016b01c069201711d02
Jonathan Steele <jonathan.steele232@gmail.com>
2019-04-24 20:26:07 -0700

    Created single-verse URLs (not all of them for sake of time). Need to get ready to make a JS-ready file.

--- go-data/main.go
+++ go-data/main.go
@@ -11,70 +11,94 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type Pair struct {
+	Left  string
+	Right int
+}
+
 func main() {
 
 	printDescriptiveStatistics()
 
+	allURLs := make([]string, 0)
+
 	// m := bookNumChaptersMap()
 	for _, bookCode := range BofMBooks {
 		var url string
-		maxVerseNum := bookNumChaptersMap()[bookCode]
-		for i := 1; i <= maxVerseNum; i++ {
+		maxChapterNum := bookNumChaptersMap()[bookCode]
+		for i := 1; i <= maxChapterNum; i++ {
 			url = buildURL(BookOfMormon, bookCode, i)
-			fmt.Println("Url: ", url)
+			allURLs = append(allURLs, url)
+			// fmt.Println("Url: ", url)
 		}
 	}
 	for _, bookCode := range DCBooks {
 		var url string
-		maxVerseNum := bookNumChaptersMap()[bookCode]
-		for i := 1; i <= maxVerseNum; i++ {
+		maxChapterNum := bookNumChaptersMap()[bookCode]
+		for i := 1; i <= maxChapterNum; i++ {
 			url = buildURL(DCTestament, bookCode, i)
-			fmt.Println("Url: ", url)
+			allURLs = append(allURLs, url)
+			// fmt.Println("Url: ", url)
 		}
 	}
 	for _, bookCode := range NTBooks {
 		var url string
-		maxVerseNum := bookNumChaptersMap()[bookCode]
-		for i := 1; i <= maxVerseNum; i++ {
+		maxChapterNum := bookNumChaptersMap()[bookCode]
+		for i := 1; i <= maxChapterNum; i++ {
 			url = buildURL(NewTestament, bookCode, i)
-			fmt.Println("Url: ", url)
+			allURLs = append(allURLs, url)
+			// fmt.Println("Url: ", url)
 		}
 	}
 	for _, bookCode := range OTBooks {
 		var url string
-		maxVerseNum := bookNumChaptersMap()[bookCode]
-		for i := 1; i <= maxVerseNum; i++ {
+		maxChapterNum := bookNumChaptersMap()[bookCode]
+		for i := 1; i <= maxChapterNum; i++ {
 			url = buildURL(OldTestament, bookCode, i)
-			fmt.Println("Url: ", url)
+			allURLs = append(allURLs, url)
+			// fmt.Println("Url: ", url)
 		}
 	}
 	for _, bookCode := range PGPBooks {
 		var url string
-		maxVerseNum := bookNumChaptersMap()[bookCode]
-		for i := 1; i <= maxVerseNum; i++ {
+		maxChapterNum := bookNumChaptersMap()[bookCode]
+		for i := 1; i <= maxChapterNum; i++ {
 			url = buildURL(PearlGPrice, bookCode, i)
-			fmt.Println("Url: ", url)
+			allURLs = append(allURLs, url)
+			// fmt.Println("Url: ", url)
 		}
 	}
 
-	// actually print the URL first.
-	// fmt.Println(url)
+	fmt.Println("Collected all Chapter URLs :: ", len(allURLs))
 
-	url := "https://www.lds.org/scriptures/pgp/a-of-f/1?lang=eng"
-	htmlBody := getHTMLfromURL(url)
+	pairs := make([]Pair, 0)
+	for _, url := range allURLs[:20] {
 
-	fmt.Println(htmlBody[:300])
+		htmlBody := getHTMLfromURL(url)
+		bodyReader := strings.NewReader(htmlBody)
+		document, err := goquery.NewDocumentFromReader(bodyReader)
+		if err != nil {
+			log.Println("Could not make a document from url: ", url, err)
+		}
 
-	// TODO use GoQuery to parse for the scriptures and find the number of verses.
-	// https://github.com/PuerkitoBio/goquery
-	bodyReader := strings.NewReader(htmlBody)
-	document, err := goquery.NewDocumentFromReader(bodyReader)
-	if err != nil {
-		log.Println("Could not make a document from url: ", url, err)
+		numVerses := len(document.Find("p .verse").Nodes)
+		fmt.Println("URL : ", url, " ==> ", numVerses)
+
+		for i := 1; i <= numVerses; i++ {
+			newURL := insertVerseNumIntoURL(url, i)
+			pairs = append(pairs, Pair{Left: newURL, Right: i})
+		}
+
 	}
+	fmt.Println("**NumPairs: ", len(pairs))
+	fmt.Println("SAMPLE PAIRS:: ", pairs[:30])
 
-	fmt.Println("Print the cached results :) ::: ", len(document.Find("p .verse").Nodes))
+}
 
+func insertVerseNumIntoURL(url string, verseNum int) string {
+
+	sArr := strings.Split(url, "?")
+	return sArr[0] + "." + strconv.Itoa(verseNum) + "?" + sArr[1]
 }
 
 // buildURL uses package-global variables from apiConstraints.go
